ada648a9896d54861b99b2a42a95630b7c603cc9
Mark Mitchell <markmitchell451@gmail.com>
2025-01-29 14:59:36 +0000

    app upd

--- src/keycloak.js
+++ src/keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from "keycloak-js";
 
 // Create a Keycloak instance with your configuration
 const keycloak = new Keycloak({
-  url: "http://auth.camelcase.club", // Keycloak base URL
+  url: "https://auth.camelcase.club", // Keycloak base URL
   realm: "my-realm", // Keycloak realm name
   clientId: "static-app", // Keycloak client ID
 });
